3ea9fb42a1567613fe9791c464ccb4a6726719ab
ArekBrodowski <106244256+ArekBrodowski@users.noreply.github.com>
2022-07-04 15:08:30 +0200

    Initial Commit

---
+++ Travel Management System/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Travel Management System
+//
+//  Created by Arek Brodowski on 04/07/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
